71c75cff9260f2161b51449e0ec61f2912d81ed3
Rodrigo dos Santos <rodrigoreiscs@gmail.com>
2025-03-09 22:05:22 +0000

    configuring eureka clients

--- hr-payroll/src/main/java/hr/payroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/hr/payroll/HrPayrollApplication.java
@@ -2,8 +2,10 @@ package hr.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients // Anotacao para habilitar o Feign, instancia disponivel para injetar
 @SpringBootApplication
 public class HrPayrollApplication {
